794505b4d06849f44dda2a2081b3def445a0845e
zhangchuanlei <1321703568@qq.com>
2017-12-05 10:11:15 +0800

    初始化项目！

---
+++ blogs-impl/src/test/java/com/blogs/BlogsImplApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blogs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogsImplApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
